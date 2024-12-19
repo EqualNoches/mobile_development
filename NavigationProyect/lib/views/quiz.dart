a4ece592e2d8ff7edcac11c8cb2ebbc571ce79eb
EqualNoches <119954229+EqualNoches@users.noreply.github.com>
2024-12-19 14:32:43 -0400

    changes to repo

--- NavigationProyect/lib/views/quiz.dart
+++ NavigationProyect/lib/views/quiz.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
 
-class Quiz extends StatelessWidget {
+class Quiz extends StatefulWidget {
   const Quiz({super.key});
 
   @override
+  State<Quiz> createState() => _QuizState();
+}
+
+class _QuizState extends State<Quiz> {
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Center(
+        child: Text("Question screen"),
+    );
   }
 }
